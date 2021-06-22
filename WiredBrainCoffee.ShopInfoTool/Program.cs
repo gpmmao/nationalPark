ef963a6deba515c4c3ce7622dd0b123fd19f1366
gpmma <gpmma@DESKTOP-JANC1G7>
2021-06-21 22:35:18 -0400

    create repository

---
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using WiredBrainCoffee.DataAccess;
+
+namespace WiredBrainCoffee.ShopInfoTool
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+
+      Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+        "write 'quit' to exit application");
+
+      var coffeeShopDataProvider = new CoffeeShopDataProvider();
+
+      while (true)
+      {
+        var line = Console.ReadLine();
+
+        if (string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+        {
+          break;
+        }
+
+        var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
+
+        var commandHandler =
+          string.Equals("help", line, StringComparison.OrdinalIgnoreCase)
+          ? new HelpCommandHandler(coffeeShops) as ICommandHandler
+          : new CoffeeShopCommandHandler(coffeeShops, line);
+
+        commandHandler.HandleCommand();
+      }
+    }
+  }
+}
